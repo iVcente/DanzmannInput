a8d7d6b4a6dd82c29e21be9558a3b16220b82f27
Vicente <ivcente@gmail.com>
2025-09-06 17:20:20 -0300

    Updated Gameplay Tags

--- Source/DanzmannInput/Private/DanzmannGameplayTags_InputActions.cpp
+++ Source/DanzmannInput/Private/DanzmannGameplayTags_InputActions.cpp
@@ -6,9 +6,6 @@ namespace Danzmann::GameplayTags
 {
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_Move, "InputAction.Move", "Represents \"Move\" Input Action.");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_Jump, "InputAction.Jump", "Represents \"Jump\" Input Action.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ActivateAim, "InputAction.Aim", "Represents \"Aim\" Input Action.");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ToggleRun, "InputAction.ToggleRun", "Represents \"Toggle Run\" Input Action.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ActivateWalk, "InputAction.ActivateWalk", "Represents \"Activate Walk\" Input Action.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ActivateCrouch, "InputAction.ActivateCrouch", "Represents \"Activate Crouch\" Input Action.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ActivateSlide, "InputAction.ActivateSlide", "Represents \"Activate Slide\" Input Action.");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputAction_ActivateAim, "InputAction.ActivateAim", "Represents \"Activate Aim\" Input Action.");
 }
